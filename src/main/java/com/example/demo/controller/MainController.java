0a1468be9e24f7d3af4495fc9171495107056737
Zaka530 <kbariev7@gmail.com>
2024-06-20 11:24:30 +0500

    .

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,16 +1,21 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.DateService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
+@AllArgsConstructor
 @RestController
     public class MainController {
 
+    private final DateService dateService;
+
         @GetMapping("/api/v1/getDate")
         public LocalDate getCurrentDate() {
-            return LocalDate.now();
+            return dateService.getCurrentDate();
         }
 
     }
